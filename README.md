3aa4d7a1e7f55e9ccecb8850160404dc187fe719
Hassanjk <git@stackblitz.com>
2024-11-18 14:53:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-components
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Hassanjk/react-components)
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pricing from './components/Pricing';
+
+function App() {
+  return <Pricing />;
+}
+
+export default App;
